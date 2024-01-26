bc2be9c06bb1030cfcfbc8f552fd111a1e55c82c
Plyampus <146366979+Plyampus@users.noreply.github.com>
2024-01-26 14:31:12 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+function checkForSpam(message) {
+  const forbiddenWords = ['spam', 'sale'];
+  const lowerCaseMessage = message.toLowerCase();
+  for (let i = 0; i < forbiddenWords.length; i++) {
+    if (lowerCaseMessage.includes(forbiddenWords[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
